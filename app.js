a191069059bd911e094976493ed95cff9f4f6ec7
Harley Reimels <reimels.harley@gmail.com>
2024-11-30 16:19:32 -0500

    Module 4 completed baseline

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var hbs = require('hbs')
 var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel')
+
+// Register partials directory
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
+
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter)
+app.get('/test', (req, res) => {
+  res.render('test');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +49,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/travel');
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+//var express = require('express');
+//var router = express.Router();
+//var controller = require('../controllers/travel');
 
 /* GET travel page. */
-router.get('/', controller.travel);
+const travel = (req, res) => { res.render('travel', { title: 'Travelr Gateways', trips})};
+//router.get('/', controller.travel);
 
-module.exports = router;
+module.exports = travel;
